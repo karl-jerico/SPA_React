f74279dd3781f5d3d334e433815b7bc1bc4c1a27
karl-jerico <jerico.jeks@gmail.com>
2023-12-27 13:32:24 +0800

    Dynamic routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import HomePage from "./pages/Home"
+import ProductDetailPage from "./pages/ProductDetail"
 import ProductPage from "./pages/Product"
 import RootLayout from "./layouts/RootLayout"
 import ErrorPage from "./pages/Error"
@@ -10,8 +11,9 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: '/', element: <HomePage /> },
-      { path: '/products', element: <ProductPage /> }
+      { path: '', element: <HomePage /> },
+      { path: 'products', element: <ProductPage /> },
+      { path: 'products/:productId', element: <ProductDetailPage /> },
     ]
   },
 ])
---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+
+const ProductDetailPage = () => {
+    const params = useParams()
+
+  return (
+    <>
+        <h1>ProductDetailPage</h1>
+        <p>{params.productId}</p>
+        <p><Link to=".." relative='path'>Back</Link></p>
+    </>
+  )
+}
+
+export default ProductDetailPage
